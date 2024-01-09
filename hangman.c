8e06a9a8f99d9fbf3c656c4c19d17891c824c2bb
felix-hauger <felix.hauger@laplateforme.io>
2024-01-09 15:05:28 +0100

    Tokenize the csv rows
    Tokenize (separate) the rows from the csv file, using commas

--- hangman.c
+++ hangman.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_CHARS 200
 
@@ -8,10 +9,12 @@ int main(int argc, char *argv[])
 
     FILE * inputFile;
     char * row;
+    char * token;
+
 
     // Testing args
     printf("You have entered %d arguments:\n", argc);
- 
+
     for (int i = 0; i < argc; i++) {
         printf("%s\n", argv[i]);
     }
@@ -27,8 +30,17 @@ int main(int argc, char *argv[])
     row = (char*)malloc(MAX_CHARS* sizeof(char));
 
     while (fgets(row, MAX_CHARS, inputFile) != NULL) {
-        printf("row: %s", row);
+        // printf("row: %s", row);
         // operations...
+        // Tokenize (splice) rows with commas (,)
+        token = strtok(row, ",");
+
+        while (token != NULL) {
+            printf("Token: %s\n", token);
+
+            // Resume tokenizing from last position
+            token = strtok(NULL, ",");
+        }
 
         free(row);
         row = (char*)malloc(MAX_CHARS* sizeof(char));
